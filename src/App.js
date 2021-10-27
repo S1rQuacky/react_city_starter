8507b93961dd1cc36e09da0f361d2ea3b980c134
Neide Urbina <neide.n.urbina@gmail.com>
2021-10-26 23:01:33 -0500

    Cities render large image when clicked, highlights selected with green border

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './index.css';
+import imagesArr from './imgData';
 
-function App() {
+// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+
+export default function App() {
+  // USE useState TO CREATE  [bigImage, setBigImage] 
+  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const firstImg = imagesArr[0].img 
+  const [images, setImages] = useState(firstImg)
+  const [border, setBorder] = useState(0)
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleClick = (clicked, noborder) => {
+    setImages(clicked);
+    setBorder(noborder) 
+   };
+
+  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const img = imagesArr.map((pic, index) => {
+    return (
+      <img className={(index === border) ? "greenthumb" : "thumb"} onClick={() => handleClick(pic.img, index)} src={pic.img} alt={pic.city} key={index} />
+    )
+  }) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cities Of The World</h1>
+      <div id="wrapper">
+        <div id="thumbnails">
+          {img}
+        </div>
+        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        <img id="bigimage" src={images} alt="bigimage" />
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
